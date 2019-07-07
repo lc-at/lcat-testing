bf59e5d76759164084288b05e03a0fb7caee5f59
loncat <me@lcat.dev>
2019-07-07 22:38:56 +0800

    Updated chall

--- ch_sqli/index.php
+++ ch_sqli/index.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/plain');
 $c = include('config.php');
 $conn = mysqli_connect($c->hostname, $c->username, $c->password);
 $ua = $_SERVER["HTTP_USER_AGENT"];
@@ -6,10 +7,11 @@ for ($i = 0; $i < 25; $i++) {
 	$ua = preg_replace("/(union|select|order|by|database|user|schema|)/i", "", $ua);
 }
 $cur_time = time();
-$res = array_values(mysqli_fetch_assoc(mysqli_query($conn, "SELECT '$ua','$cur_time' FROM DUAL")));
+$stmt = "SELECT concat('$ua'), '$cur_time' FROM DUAL";
+$res = array_values(mysqli_fetch_assoc(mysqli_query($conn, $stmt)));
 $ua_o = htmlentities($res[0]);
 if(abs($res[1] - $cur_time) < 2) {
 	echo sprintf("At this time ($cur_time), you have a very good browser, ua ~> %s", $ua_o);
 }
-echo "<hr>";
-highlight_file(__FILE__);
+echo str_repeat(PHP_EOL, 3);
+echo file_get_contents(__FILE__);
