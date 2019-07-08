e2b206faffd1646a3a505537af4aaf5a24877d99
loncat <me@lcat.dev>
2019-07-08 14:32:12 +0800

    Updated chall

--- ch_sqli/index.php
+++ ch_sqli/index.php
@@ -1,17 +1,28 @@
 <?php
 header('Content-Type: text/plain');
+session_start();
 $c = include('config.php');
 $conn = mysqli_connect($c->hostname, $c->username, $c->password);
 $ua = $_SERVER["HTTP_USER_AGENT"];
-for ($i = 0; $i < 25; $i++) {
+
+for ($i = 0; $i < rand(25, 28); $i++) {
 	$ua = preg_replace("/(union|select|order|by|database|user|schema|)/i", "", $ua);
 }
+
 $cur_time = time();
 $stmt = "SELECT concat('$ua'), '$cur_time' FROM DUAL";
 $res = array_values(mysqli_fetch_assoc(mysqli_query($conn, $stmt)));
 $ua_o = htmlentities($res[0]);
-if(abs($res[1] - $cur_time) < 2) {
+
+if ($res[1] == $cur_time) {
 	echo sprintf("At this time ($cur_time), you have a very good browser, ua ~> %s", $ua_o);
+} else {
+	$_SESSION['blocked'] = true;
 }
+
+if (isset($_SESSION['blocked'])) {
+	die('Session blocked. Request rejected.');
+}
+
 echo str_repeat(PHP_EOL, 3);
 echo file_get_contents(__FILE__);
